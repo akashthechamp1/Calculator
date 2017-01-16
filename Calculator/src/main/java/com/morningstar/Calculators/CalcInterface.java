c0da07f2ba7668e84ddeb597f2f081965f762623
mandar-deshpande <mandar.deshpande@ves.ac.in>
2017-01-16 17:41:02 +0530

    I Just commited My Repo

---
+++ Calculator/src/main/java/com/morningstar/Calculators/CalcInterface.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package com.morningstar.Calculators;
+
+/**
+ * @author mdeshpa
+ *
+ */
+public interface CalcInterface {
+
+	public Double add(double input1, double input2);
+
+	public Double subtract(double input1, double input2);
+
+	public Double multiply(double input1, double input2);
+
+	public Double divide(double input1, double input2);
+
+	public Double expone(double input1);
+
+	public Double nroot(double input1, double input2);
+
+	public double[] qroot(double input1, double input2, double input3);
+
+	public double logc(double a);
+
+	public double mod(double input1, double input2);
+
+	public double GeoTan(double input1);
+
+	public Double display_PI();
+
+	public Double sin(double d);
+
+	public Double cos(double d);
+}
